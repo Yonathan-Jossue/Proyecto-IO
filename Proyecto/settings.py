a782b1b74d210cee82dbdf425c3691eedca74132
Yonathan Jossue <yonathan15mora@gmail.com>
2025-10-24 12:19:19 -0600

    nuevo settings

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -117,5 +117,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Carpeta donde Django pondrá los archivos listos para producción
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Carpeta donde están tus CSS/JS durante desarrollo
+    os.path.join(BASE_DIR, 'app', 'static'),
 ]
